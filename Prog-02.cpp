8e167e42549449766e00a7a6bfff0b3b08a1b1da
Er. Varinder Singh <varinder.sgc.er@gmail.com>
2026-08-07 15:46:38 +0530

    Refactor array insertion and deletion code

--- Prog-02.cpp
+++ Prog-02.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int a[20]= {
-        10,20,30,40,50
-    }
-    ,n=5,pos=2,value=25;
-    for(int i=n;
-    i>pos;
-    i--) a[i]=a[i-1];
-    a[pos]=value;
+    int a[20] = {10, 20, 30, 40, 50}, n = 5, pos = 2, value = 25;
+
+    // Insertion
+    for (int i = n; i > pos; i--)
+        a[i] = a[i - 1];
+
+    a[pos] = value;
     n++;
-    cout<<"After insertion: ";
-    for(int i=0;
-    i<n;
-    i++) cout<<a[i]<<" ";
-    pos=3;
-    for(int i=pos;
-    i<n-1;
-    i++) a[i]=a[i+1];
+
+    cout << "After insertion: ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    // Deletion
+    pos = 3;
+    for (int i = pos; i < n - 1; i++)
+        a[i] = a[i + 1];
+
     n--;
-    cout<<"After deletion: ";
-    for(int i=0;
-    i<n;
-    i++) cout<<a[i]<<" ";
+
+    cout << "\nAfter deletion: ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
     return 0;
 }
