5c5035b83a5ad5b4e2ad1d3c48230c7c2c5dbc98
Er. Varinder Singh <varinder.sgc.er@gmail.com>
2026-08-07 15:59:16 +0530

    Refactor array insertion and deletion in Prog-02.cpp

--- Prog-02.cpp
+++ Prog-02.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a[20] = {10, 20, 30, 40, 50}, n = 5, pos = 2, value = 25;
+int main()
+{
+    // Initial array
+    int arr[20] = {10, 20, 30, 40, 50};
+    int n = 5;
 
-    // Insertion
+    // -------- Insertion --------
+    int pos = 2;      // Position to insert (0-based index)
+    int value = 25;   // Value to insert
+
+    // Shift elements one position to the right
     for (int i = n; i > pos; i--)
-        a[i] = a[i - 1];
+    {
+        arr[i] = arr[i - 1];
+    }
 
-    a[pos] = value;
+    // Insert the new value
+    arr[pos] = value;
     n++;
 
-    cout << "After insertion: ";
+    cout << "After Insertion: ";
     for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
+    {
+        cout << arr[i] << " ";
+    }
 
-    // Deletion
-    pos = 3;
+    // -------- Deletion --------
+    pos = 3;   // Position to delete
+
+    // Shift elements one position to the left
     for (int i = pos; i < n - 1; i++)
-        a[i] = a[i + 1];
+    {
+        arr[i] = arr[i + 1];
+    }
 
     n--;
 
-    cout << "\nAfter deletion: ";
+    cout << "\nAfter Deletion: ";
     for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
