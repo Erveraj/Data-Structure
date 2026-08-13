7f1356c5259b1a1401f3f6c5500ed53b83d8b1bb
Everaj <varinder.sgc.er@gmail.com>
2026-08-13 14:24:01 +0530

    Fixed

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,10 +1,13 @@
+// C++ program to demonstrate the use of pointers
+
 #include<iostream>
 using namespace std;
 int main(){
 	string food = "Pizza";
 	cout << food << endl;
 	cout << &food << endl;
-
+	
+// create a pointer variable that holds the address of food
 	string* ptr = &food;
 	cout << &ptr << endl;
 	cout << ptr << endl;
