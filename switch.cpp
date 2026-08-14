602d5f72bc5940840db244c115bd6f5f2d1636a2
Everaj <varinder.sgc.er@gmail.com>
2026-08-14 12:07:24 +0530

    C++ program to demonstrate the use of switch statement

---
+++ switch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    
+    cout << "Enter your age: ";
+    cin >> age;
+    
+    switch (age >= 18) {
+        case 1:  // age >= 18 is true
+            cout << "You are an Adult" << endl;
+            break;
+        case 0:  // age >= 18 is false
+            cout << "You are a Child" << endl;
+            break;
+        default:
+            cout << "Invalid input" << endl;
+    }
+    
+    return 0;
+}
