c9a26f6a7980b59a8ad55d328e2afd1cecae551c
Everaj <varinder.sgc.er@gmail.com>
2026-08-20 09:49:02 +0530

    Add new code

--- Stack.cpp
+++ Stack.cpp
@@ -25,6 +25,25 @@ cout << "Is stack empty? " << (car.empty() ? "Yes" : "No") << endl;
 car.pop();
 cout << "Top Element after pop: "<< car.top() << endl;
 
+/*
+  // Create a stack of strings called cars
+  stack<string> cars;
+
+  // Add elements to the stack
+  cars.push("Volvo");
+  cars.push("BMW");
+  cars.push("Ford");
+  cars.push("Mazda");
+  
+  // Change the value of the top element
+  cars.top() = "Tesla";
+
+  // Access the top element
+  cout << cars.top();
+  return 0;
+
+*/
+
 }
 
 /*
